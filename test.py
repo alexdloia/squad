14ad0a9d67e10c000e7557132bd1262fce3867e0
Antonio Ferris <antonioferris01@gmail.com>
2022-03-06 00:45:39 -0800

    k oracle bugfix 3

--- test.py
+++ test.py
@@ -116,6 +116,7 @@ def main(args):
                 log_p1, log_p2 = util.convert_probs(logprob_chunks, candidates, c_len, c_mask, device)
             elif args.K_oracle != 0:
                 candidates = util.generate_candidates(model, cw_idxs, qw_idxs, pos_idxs, ner_idxs, bem_idxs, (y1, y2), args.K_oracle, device, train=False)
+                logprob_chunks = model(cw_idxs, qw_idxs, pos_idxs, ner_idxs, bem_idxs, candidates)
                 some_log_p1, some_log_p2 = util.convert_probs(logprob_chunks, candidates, c_len, c_mask, device)
                 log_p1, log_p2 = torch.zeros(batch_size, p_len), torch.zeros(batch_size, p_len)
                 log_p1 = log_p1.to(device)
